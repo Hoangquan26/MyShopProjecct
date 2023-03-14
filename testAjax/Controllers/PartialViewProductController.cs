fff2222f1691c727c5dbc5bc09b799a739f859a9
hquan26122003 <hquan26122003@gmail.com>
2023-03-15 00:05:06 +0700

    aferSertupDb

---
+++ testAjax/Controllers/PartialViewProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testAjax.Models;
+
+namespace testAjax.Controllers
+{
+    public class PartialViewProductController : Controller
+    {
+        // GET: ParitalViewProduct
+        public PartialViewResult ProductPage(int? maTheLoai = null)
+        {
+            var products = ProductAction.loadProduct().ToList();
+            if (maTheLoai != null)
+            {
+                var items = products.Where(item => item.theLoaiSanPham == maTheLoai).ToList();
+                products = items.ToList();
+            }
+            return PartialView(products);
+        }
+    }
+}
